b09c4c64a46ad780766ee3c63ffa17be30d93efc
tanish <tanish@gmail.com>
2024-08-05 15:56:59 +0530

    Changed Color for code editor

--- src/components/Codeeditor/Codeeditor.jsx
+++ src/components/Codeeditor/Codeeditor.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Codeeditor.css";
 import Inputoutput from "./Input-Output/Inputoutput.jsx";
+// import { BsShare } from "react-icons/bs";
+// import { AiOutlineClear } from "react-icons/ai";
 
 const Codeeditor = () => {
   return (
     <div className="codeeditor">
       <div className="main">
-        <div className="toolbox"></div>
+        <div className="toolbox">
+          <div className="filename">
+            main.lang
+          </div>
+          <div className="toolbox-btns">
+            <button className="toolbox-btn">
+              Share
+            </button>
+            <button className="toolbox-btn">
+              Clear
+            </button>
+            <button className="toolbox-btn">Run</button>
+          </div>
+        </div>
         <div className="code"></div>
       </div>
       <div className="user">
