44de71a86a9d55e8c2de21f5683a0b33843caaac
tanish <tanish@gmail.com>
2024-08-09 20:15:27 +0530

    css

--- src/components/Codeeditor/Codeeditor.jsx
+++ src/components/Codeeditor/Codeeditor.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Codeeditor.css";
 import Inputoutput from "./Input-Output/Inputoutput.jsx";
 // import { BsShare } from "react-icons/bs";
 // import { AiOutlineClear } from "react-icons/ai";
 
 const Codeeditor = () => {
+
+  const [lang, setLang] = useState("");
+
+  const languages = {
+    "C++": ".cpp",
+    Java: ".java",
+    Javascript: '.js',
+    Python: ".py",
+    Typescript: ".ts"
+  };
+
+  const handleChange = (event) => {
+    setLang(languages[event.target.value]);
+  };
+
   return (
     <div className="codeeditor">
 
@@ -12,9 +27,29 @@ const Codeeditor = () => {
 
         <div className="toolbox">
           <div className="filename">
-            main.lang
+            <div className="language">
+              main{lang}
+            </div>
           </div>
+          <div className="code-editor-nav">
+            {/*  */}
 
+            <div>
+              <select id="language-select" value={lang} onChange={handleChange}>
+                <option defaultValue="cpp">Choose Language</option>
+                {Object.keys(languages).map((key) => (
+                  <option key={key} value={key}>
+                    {key}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/*  */}
+            <button>Share</button>
+            <button>Run</button>
+            <button>Clear</button>
+          </div>
         </div>
         <div className="code"></div>
       </div>
